5c4fc86393ba793b118c5723fd549a4cda819956
sinduvaddi <83584997+sinduvaddi@users.noreply.github.com>
2023-04-23 20:48:25 +0530

    Create Login Form

--- src/App.js
+++ src/App.js
@@ -1,36 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 export default function App() {
-  const [data, setData] = useState({
-    username: '',
-    password: '',
-  });
-  const { username, password } = data;
-  const changeHandler = (e) => {
-    setData({ ...data, [e.target.name]: [e.target.value] });
-  };
-  return (
-    <div>
-      <center>Grettings , Signin to view your portfolio!!!</center>
+  const inititalValues = { email: '', password: '' };
+  const [formValues, setFormValues] = useState(inititalValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-      <form>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={changeHandler}
-        />{' '}
-        <br />
-        <input
-          type="password"
-          name="password"
-          value={username}
-          onChange={changeHandler}
-        />{' '}
-        <br />
-        <input type="submit" name="submit" />
-      </form>
-    </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validateForm(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (isSubmit) {
+      setFormErrors(validateForm(formValues));
+    }
+  }, [formErrors]);
+
+  const validateForm = (formValues) => {
+    const errors = {};
+    if (!formValues.email) {
+      errors.email = 'Email is required!';
+    }
+    if (!formValues.password) {
+      errors.password = 'Password is required!';
+    } else if (formValues.password.length < 6) {
+      errors.password = 'Password must be more than 6 characters';
+    } else if (formValues.password.length > 10) {
+      errors.password = 'Password cannot exceed more than 10 characters';
+    }
+    return errors;
+  };
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <h1>Login Form</h1>
+          <div className="loginForm">
+            <div className="field">
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                value={formValues.email}
+                onChange={handleChange}
+              />
+            </div>
+            <p class="errorTxt">{formErrors.email}</p>
+            <div className="field">
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                value={formValues.password}
+                onChange={handleChange}
+              />
+            </div>
+            <p class="errorTxt">{formErrors.password}</p>
+            <button className="submitBtn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="detailsSection">
+        <h2>Entered Details</h2>
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      </div>
+    </React.Fragment>
   );
 }
